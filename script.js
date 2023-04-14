360d1a8ac31ebaadc27c14167cd72df5de3efac2
bigolboyyo <wesley.grant94@gmail.com>
2023-04-14 17:07:43 -0400

    up and down

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
-// Set the canvas size to fill the window
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
@@ -11,7 +10,7 @@ let circles = [
     y: canvas.height / 2,
     radius: 50,
     color: "white",
-    velocityX: Math.random() * 10 - 5, // Random velocity between -5 and 5
+    velocityX: Math.random() * 10 - 5,
     velocityY: Math.random() * 10 - 5,
   },
   {
@@ -19,10 +18,16 @@ let circles = [
     y: canvas.height / 4,
     radius: 30,
     color: "red",
-    velocityX: Math.random() * 10 - 5, // Random velocity between -5 and 5
+    velocityX: Math.random() * 10 - 5,
     velocityY: Math.random() * 10 - 5,
   },
 ];
+
+let numBalls = circles.length;
+let addingBalls = true;
+let removingBalls = false;
+let reverseTimer = 0;
+let shouldReset = false;
 
 function drawCircle(circle) {
   ctx.beginPath();
@@ -44,7 +49,7 @@ function checkCollision(circle1, circle2) {
     circle2.velocityX = -circle2.velocityX;
     circle2.velocityY = -circle2.velocityY;
 
-    if (circles.length < 50) {
+    if (addingBalls && numBalls < 50) {
       let newX = Math.random() * canvas.width;
       let newY = Math.random() * canvas.height;
       let newRadius = 15;
@@ -70,20 +75,22 @@ function checkCollision(circle1, circle2) {
           y: newY,
           radius: newRadius,
           color: generateRandomGradient(),
-          velocityX: Math.random() * 10 - 5, // Random velocity between -5 and 5
+          velocityX: Math.random() * 10 - 5,
           velocityY: Math.random() * 10 - 5,
         });
+        numBalls++;
       }
+    } else if (!addingBalls && numBalls > 2 && !removingBalls) {
+      removingBalls = true;
+      reverseTimer = 5000 / (numBalls - 2);
     }
   }
 }
 
 function generateRandomGradient() {
-  // Generate two random color stops
   const color1 = getRandomColor();
   const color2 = getRandomColor();
 
-  // Create the gradient with the randomly generated color stops
   const gradient = ctx.createLinearGradient(0, 0, canvas.width, canvas.height);
   gradient.addColorStop(0, color1);
   gradient.addColorStop(1, color2);
@@ -92,7 +99,6 @@ function generateRandomGradient() {
 }
 
 function getRandomColor() {
-  // Generate a random color in the format 'rgb(r, g, b)'
   return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(
     Math.random() * 256
   )}, ${Math.floor(Math.random() * 256)})`;
@@ -104,13 +110,11 @@ function update() {
   for (let i = 0; i < circles.length; i++) {
     let circle1 = circles[i];
 
-    // Check for collision with other circles
     for (let j = i + 1; j < circles.length; j++) {
       let circle2 = circles[j];
       checkCollision(circle1, circle2);
     }
 
-    // Check for collision with walls
     if (
       circle1.x + circle1.radius > canvas.width ||
       circle1.x - circle1.radius < 0
@@ -125,11 +129,52 @@ function update() {
       circle1.velocityY = -circle1.velocityY;
     }
 
-    // Update the position of the circle based on its velocity
     circle1.x += circle1.velocityX;
     circle1.y += circle1.velocityY;
 
     drawCircle(circle1);
+  }
+
+  if (addingBalls && numBalls == 50) {
+    addingBalls = false;
+    removingBalls = true;
+    reverseTimer = 5000 / (numBalls - 2);
+  } else if (removingBalls && numBalls > 2 && reverseTimer > 0) {
+    reverseTimer -= 16;
+    if (reverseTimer <= 0) {
+      circles.splice(-1, 1);
+      numBalls--;
+      reverseTimer = 5000 / (numBalls - 2);
+    }
+  } else if (removingBalls && numBalls == 2) {
+    removingBalls = false;
+    addingBalls = true;
+    shouldReset = true;
+  }
+
+  if (shouldReset) {
+    circles = [
+      {
+        x: canvas.width / 2,
+        y: canvas.height / 2,
+        radius: 50,
+        color: "white",
+        velocityX: Math.random() * 10 - 5,
+        velocityY: Math.random() * 10 - 5,
+      },
+      {
+        x: canvas.width / 4,
+        y: canvas.height / 4,
+        radius: 30,
+        color: "red",
+        velocityX: Math.random() * 10 - 5,
+        velocityY: Math.random() * 10 - 5,
+      },
+    ];
+    numBalls = circles.length;
+    addingBalls = true;
+    removingBalls = false;
+    shouldReset = false;
   }
 
   requestAnimationFrame(update);
